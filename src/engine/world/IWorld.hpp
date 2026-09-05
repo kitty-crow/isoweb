a525161d93fbe0da7c419c41a470b611d822893b
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-05 22:28:43 +0100

    Expose generic navigation connections

--- src/engine/world/IWorld.hpp
+++ src/engine/world/IWorld.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "engine/math/Vec3.hpp"
+#include "engine/navigation/NavigationConnection.hpp"
 #include "engine/render/Ray.hpp"
 #include "engine/world/Character.hpp"
 #include "engine/world/WorldObject.hpp"
@@ -22,9 +23,12 @@ public:
   virtual ~IWorld() = default;
 
   virtual const WorldBounds& bounds() const = 0;
+  virtual const WorldBounds& boundsForLevel(const std::string& levelId) const = 0;
   virtual Vec3 sample(const Ray& ray, float backgroundY) const = 0;
   virtual const std::vector<Object>& objects() const = 0;
+  virtual const std::vector<Object>& objectsForLevel(const std::string& levelId) const = 0;
   virtual const std::vector<Character>& characters() const = 0;
+  virtual const std::vector<NavigationConnection>& navigationConnections() const = 0;
   virtual bool intersectsSolid(const HitBox& hitBox) const = 0;
   virtual bool collidesWith(const Object& candidate) const = 0;
 
