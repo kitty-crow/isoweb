a62dcfe7291e9de686fa8a2a5d13b3c3c75f68a0
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-05 22:23:54 +0100

    Expose generic character runtime state

--- src/engine/world/IWorld.hpp
+++ src/engine/world/IWorld.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
 #include <vector>
 
 #include "engine/math/Vec3.hpp"
 #include "engine/render/Ray.hpp"
+#include "engine/world/Character.hpp"
 #include "engine/world/WorldObject.hpp"
 
 namespace isoweb {
@@ -22,12 +24,16 @@ public:
   virtual const WorldBounds& bounds() const = 0;
   virtual Vec3 sample(const Ray& ray, float backgroundY) const = 0;
   virtual const std::vector<Object>& objects() const = 0;
+  virtual const std::vector<Character>& characters() const = 0;
   virtual bool intersectsSolid(const HitBox& hitBox) const = 0;
   virtual bool collidesWith(const Object& candidate) const = 0;
 
   virtual std::size_t levelCount() const = 0;
   virtual std::size_t activeLevelIndex() const = 0;
   virtual std::size_t defaultLevelIndex() const = 0;
+  virtual const std::string& activeLevelId() const = 0;
+
+  virtual float baseMovementSpeed() const = 0;
 };
 
 } // namespace engine
