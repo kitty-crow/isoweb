e9eba3fdcc20711881377a6f81f31c15a22ff91d
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-05 22:35:34 +0100

    Expose demo character state and movement bridge

--- src/wasm/exports.cpp
+++ src/wasm/exports.cpp
@@ -82,3 +82,61 @@ extern "C" EMSCRIPTEN_KEEPALIVE int isoweb_active_level_index() {
 extern "C" EMSCRIPTEN_KEEPALIVE int isoweb_default_level_index() {
   return static_cast<int>(application().defaultLevelIndex());
 }
+
+extern "C" EMSCRIPTEN_KEEPALIVE void isoweb_state_begin(float baseMovementSpeed) {
+  application().resetRuntimeState(baseMovementSpeed);
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE void isoweb_state_add_character(
+  float x,
+  float y,
+  float z,
+  float forwardX,
+  float forwardY,
+  float width,
+  float depth,
+  float height,
+  int levelIndex,
+  int solid,
+  int npc,
+  int controllable,
+  float speedMultiplier,
+  float selectionRed,
+  float selectionGreen,
+  float selectionBlue
+) {
+  application().addCharacter(
+    x,
+    y,
+    z,
+    forwardX,
+    forwardY,
+    width,
+    depth,
+    height,
+    levelIndex,
+    solid != 0,
+    npc != 0,
+    controllable != 0,
+    speedMultiplier,
+    selectionRed,
+    selectionGreen,
+    selectionBlue
+  );
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE int isoweb_character_count() {
+  return static_cast<int>(application().characterCount());
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE void isoweb_pointer_action(float normalisedX, float normalisedY) {
+  application().pointerAction(normalisedX, normalisedY);
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE void isoweb_clear_selection() {
+  application().clearSelection();
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE void isoweb_tick(float deltaSeconds) {
+  application().tick(deltaSeconds);
+}
