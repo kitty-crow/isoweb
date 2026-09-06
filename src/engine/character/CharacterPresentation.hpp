40ff8d8e0a14de837e6bcea2e9eb24aea70d7276
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-06 12:16:18 +0100

    Separate presentation from animation timing policy

--- src/engine/character/CharacterPresentation.hpp
+++ src/engine/character/CharacterPresentation.hpp
@@ -19,28 +19,17 @@ class CharacterPresentationPolicy {
 public:
   virtual ~CharacterPresentationPolicy() = default;
   virtual CharacterFacing facing(const Character& character, const Camera& camera) const = 0;
-  virtual float framesPerSecond(const Character& character, const SpriteAnimation& animation, float effectiveMovementSpeed, float baseMovementSpeed) const = 0;
 };
 
 class DefaultCharacterPresentationPolicy final : public CharacterPresentationPolicy {
 public:
   CharacterFacing facing(const Character& character, const Camera& camera) const override;
-  float framesPerSecond(const Character& character, const SpriteAnimation& animation, float effectiveMovementSpeed, float baseMovementSpeed) const override;
 };
 
 CharacterPresentation resolveCharacterPresentation(
   const Character& character,
   const Camera& camera,
-  const CharacterPresentationPolicy& policy,
-  float effectiveMovementSpeed,
-  float baseMovementSpeed
-);
-
-void advanceCharacterAnimation(
-  Character& character,
-  float deltaSeconds,
-  const SpriteAnimation* animation,
-  float framesPerSecond
+  const CharacterPresentationPolicy& policy
 );
 
 } // namespace engine
