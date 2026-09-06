5bdd967cccb9017d1b5c6908a437b37bb98f215e
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-06 11:36:25 +0100

    Expose replaceable navigation and interaction policies

--- src/engine/character/CharacterPolicies.hpp
+++ src/engine/character/CharacterPolicies.hpp
@@ -7,6 +7,13 @@ namespace engine {
 
 class World;
 
+struct CharacterEngineDefaults {
+  float baseMovementSpeed = 1.45f;
+  float navigationCellSize = 0.25f;
+  float arrivalEpsilon = 0.025f;
+  float destinationSearchRadius = 2.5f;
+};
+
 class CollisionPolicy {
 public:
   virtual ~CollisionPolicy() = default;
@@ -18,7 +25,24 @@ public:
 class DestinationPolicy {
 public:
   virtual ~DestinationPolicy() = default;
-  virtual EntityLocation resolve(const World& world, const Character& character, const EntityLocation& requested) const;
+  virtual EntityLocation resolve(
+    const World& world,
+    const Character& character,
+    const EntityLocation& requested,
+    const CharacterEngineDefaults& defaults
+  ) const;
+};
+
+class NavigationPolicy {
+public:
+  virtual ~NavigationPolicy() = default;
+  virtual bool buildRoute(
+    const World& world,
+    const Character& character,
+    const EntityLocation& destination,
+    const CharacterEngineDefaults& defaults,
+    CharacterMovementState& route
+  ) const = 0;
 };
 
 class MovementPolicy {
@@ -29,11 +53,23 @@ public:
   }
 };
 
-struct CharacterEngineDefaults {
-  float baseMovementSpeed = 1.45f;
-  float navigationCellSize = 0.25f;
-  float arrivalEpsilon = 0.025f;
-  float destinationSearchRadius = 2.5f;
+class InteractionPolicy {
+public:
+  virtual ~InteractionPolicy() = default;
+  virtual bool canCommand(const Character& character) const {
+    return character.controllable;
+  }
+};
+
+class DefaultNavigationPolicy final : public NavigationPolicy {
+public:
+  bool buildRoute(
+    const World& world,
+    const Character& character,
+    const EntityLocation& destination,
+    const CharacterEngineDefaults& defaults,
+    CharacterMovementState& route
+  ) const override;
 };
 
 } // namespace engine
